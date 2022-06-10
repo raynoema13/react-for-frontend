b163bfc717b1f10e2195ce95335852c003018fb8
raynoema13 <90075955+raynoema13@users.noreply.github.com>
2022-06-10 01:40:58 -1000

    Add code to get token when user logs in

--- app.js
+++ app.js
@@ -15,4 +15,19 @@ app.post('/api/posts', (req, res) => {
     });
 });
 
+app.post('/api/login', (req, res) => {
+    //Mock user
+    const user = {
+        id: 1,
+        username: 'ray',
+        email: 'ray@react.com'
+    }
+    //send a token
+    jwt.sign({user}, 'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
 app.listen(5005, () => console.log('Server started on port 5000'));
